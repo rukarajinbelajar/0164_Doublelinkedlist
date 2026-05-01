e187fb613b095fdc1f9701b65a3ca24c19a42844
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-01 16:14:13 +0700

    membuat void hapus

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -42,3 +42,46 @@ public:
                 cout << "\nDuplicate number not allowed" << endl;
                 return;
             }
+            // Step 4: newNode.next = START
+            newNode->next = START;
+
+            // Step 5: START.prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev = newNode;
+
+            // Step 6: newNode.prev = NULL
+            newNode->prev = NULL;
+
+            // Step 7: START = newNode
+            START = newNode;
+            return;
+        }
+
+        // insert in between Node
+        // Step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed" << endl;
+            return;
+        }
+
+        // Step 9: Insert between current and current->next
+        newNode->next = current->next; //Step 9a: newNode.next = current.next
+        newNode->prev = current;      // Step 9b : newNode.prev = current
+
+        // insert Last Node
+        if (current->next != NULL)
+            current->next->prev = newNode; // Step 9c: current.next.prev = newNode
+
+        current->next = newNode; // Step 9d: current.next = newNode
+    }
+
+    void hapus()
+    
+
